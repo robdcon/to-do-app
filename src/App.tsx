f80ee075c6da11d2595372e827f3c4cd5b11c976
robdcon <robdcon@gmail.com>
2020-01-28 12:17:36 +0000

    adds hover effect to todo

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,34 @@ import ToDoContainer from './components/ToDoContainer';
 import Button from './components/Button';
 import ToDo from './components/ToDo';
 
+const toDoList: Array<ToDo> = [
+
+  {
+    text: 'To Do One',
+    status: 'done'
+  },
+  {
+    text: 'To Do Two',
+    status: 'pending'
+  },
+  {
+    text: 'To Do Three',
+    status: 'pending'
+  },
+
+]
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <ToDoContainer>
-        <ToDo text="To Do One" status="pending"/>
+        {
+          toDoList.map((todo) => {
+
+            return <ToDo text={todo.text} status={todo.status} />
+
+          })
+        }
         <Button text="Click Me" handleClick={() => console.log('Hello World!!!')}/>
       </ToDoContainer>
     </div>
--- src/components/ToDo/ToDo.styles.js
+++ src/components/ToDo/ToDo.styles.js
@@ -5,8 +5,13 @@ export const StyledToDo = styled.div`
  display: flex;
  justify-content:center;
  align-items:center;
- box-shadow: 9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px  rgba(255,255,255, 0.5);
+ box-shadow: 9px 9px 10px rgb(163,177,198,0.6), -9px -9px 10px  rgba(255,255,255, 0.5);
  min-width:300px;
  min-height:100px;
+ transition: all 3s;
+
+ &:hover {
+    box-shadow: inset 9px 9px 10px rgb(163,177,198,0.6), inset -9px -9px 10px  rgba(255,255,255, 0.5);
+ }
 
 `;
