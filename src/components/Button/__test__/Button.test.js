c13f81907f1d5410377f45d4b5bd50046ad45b92
robdcon <robdcon@gmail.com>
2020-01-28 16:47:25 +0000

    adds jest-dom, 2 tests on button working

--- src/components/Button/__test__/Button.test.js
+++ src/components/Button/__test__/Button.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './../Button';
 import { render } from '@testing-library/react';
-import 'jest-dom/extend-expect';
+
 
 it("renders without crashing", () => {
 
