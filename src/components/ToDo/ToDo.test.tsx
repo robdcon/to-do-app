2eb1f9674dacbf0c82dc34cb03830e83d5ca03a2
robdcon <robdcon@gmail.com>
2020-01-28 14:09:27 +0000

    adds-first-test

--- src/components/ToDo/ToDo.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import ToDo from './ToDo';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<ToDo text="test" status="pending" />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
