b372f90cd0023f15730178a8b4b770375c851569
robdcon <robdcon@gmail.com>
2020-01-28 14:38:58 +0000

    adds new test

--- src/components/Button/__test__/Button.test.js
+++ src/components/Button/__test__/Button.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './../Button';
-import isTSAnyKeyword from '@babel/types';
+import { render } from '@testing-library/react';
+import 'jest-dom/extend-expect';
 
 it("renders without crashing", () => {
 
     const div = document.createElement('div')
     ReactDOM.render(<Button></Button>, div)
+})
+
+it("renders button correctly", () => {
+
+    const {getByTestId} = render(<Button text="Click Me"></Button>)
+    expect(getByTestId('add-btn')).toHaveTextContent("Click Me")
 })
