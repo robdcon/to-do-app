b4f3c78d763464d25df9f4caccaa6db595380ca0
robdcon <robdcon@gmail.com>
2020-01-30 22:26:10 +0000

    adds styles to todolist

--- src/components/ToDoList/ToDoList.styles.js
+++ src/components/ToDoList/ToDoList.styles.js
@@ -1,6 +1,16 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// export const Test = styled.div`
-//  display: flex;
-// `;
-//
+export const StyledToDoList = styled.ul`
+
+    max-width: 600px;
+    margin:0 auto;
+    li {
+        width:100%;
+        display:flex;
+        list-style:none;
+        margin: 1rem 0;
+
+        label{width:100%;}
+        
+    }
+`;
--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ToDo from '../ToDo'
+import { StyledToDoList } from './ToDoList.styles';
 //import { Test } from './ToDoList.styles';
 
 interface ToDoListProps {
@@ -12,13 +13,13 @@ interface ToDoListProps {
 const ToDoList: React.FC<ToDoListProps> = ({ todos, toggleToDo}) => {
 
    return ( 
-      <ul>
+      <StyledToDoList>
         {
           todos.map((todo) => {
             return <li key={todo.text}><ToDo todo={todo} toggleToDo={toggleToDo} /></li>
           })
         }
-      </ul>
+      </StyledToDoList>
     )
 }
 
