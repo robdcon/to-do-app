541131d56b6a76742fce1e2cb948118185efc5ac
robdcon <robdcon@gmail.com>
2020-02-04 22:42:42 +0000

    Refactors app

--- src/components/ToDoContainer/ToDoContainer.styles.js
+++ src/components/ToDoContainer/ToDoContainer.styles.js
@@ -2,14 +2,16 @@ import styled from 'styled-components';
 
 
 
-export const StyledContainer = styled.div`
- height:100vh;
- width:100vw;
- display: flex;
- flex-direction: column;
- justify-content:space-around;
- align-items:center;
- overflow:hidden;
- background-color: #e0e5ec;
+export const StyledToDoContainer = styled.div`
+
+    height:100vh;
+    width:100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content:flex-start;
+    align-items:center;
+    background-color: #ffffff;
+    padding: 3%;;
+    box-sizing: border-box;
 `;
 
